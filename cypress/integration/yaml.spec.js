f6e6acdb21e1aae9e610bc25ae7f7acf86e01e31
snapperben <info@brhd.co.uk>
2019-11-04 16:22:03 +0000

    Cypress test now read in the config.yaml file (which defines the domain and flagpole data file) to constrain its tests

---
+++ cypress/integration/yaml.spec.js
@@ -0,0 +1,19 @@
+describe('Basic flagpole tests using config.yaml....', function () {
+  const env = Cypress.env('ENVIRONMENT'),
+    YAML = require('yamljs');
+
+  it("Show the initial page from the flagpole data", function () {
+    cy.readFile('config.yaml').then((str) => {
+      const config = YAML.parse(str);
+      let baseURL = config[env].domain;
+      cy.readFile(config[env]['source']).then(function (data) {
+        let flagpoleKeys = Object.keys(data)
+        cy.visit(baseURL).then(function () {
+          cy.get('.flagpole-container').should('have.length', flagpoleKeys.length)
+        })
+      })
+    })
+  })
+})
+
+
