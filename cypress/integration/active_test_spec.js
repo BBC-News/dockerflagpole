9592a5793571ddfeb97cba03d7819ee33315c4ce
snapperben <info@brhd.co.uk>
2019-11-08 10:38:09 +0000

    Update Cypress tests to utilize the new information of a flagpole and to check that modified date is correctly operated

--- cypress/integration/active_test_spec.js
+++ cypress/integration/active_test_spec.js
@@ -22,49 +22,107 @@ describe('Active flagpole tests using config.yaml....', function () {
     it("Use edit controls for each flagpole with no update to current value", function () {
       for (let i = 0; i < flagpoleKeys.length; i++) {
         let flagpoleNameText = flagpoleKeys[i].toUpperCase(),
-          flagpoleValue = flagpoleData[flagpoleKeys[i]],
-          flagpoleValueText = flagpoleValue?'TRUE':'FALSE';
+          flagpole = flagpoleData[flagpoleKeys[i]],
+          flagpoleValue = flagpole.value,
+          flagpoleValueText = flagpoleValue?flagpole.trueName:flagpole.falseName,
+          trueSelector = '[name="' + flagpoleNameText + '"][data-check-role="' + flagpole.trueName + '"]',
+          falseSelector = '[name="' + flagpoleNameText + '"][data-check-role="' + flagpole.falseName + '"]';
+
         if (flagpoleValue) {
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="OFF"]').check();
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="ON"]:checked').should('not.exist');
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="OFF"]:checked').should('exist');
+          cy.get(falseSelector).check();
+          cy.get(trueSelector + ':checked').should('not.exist');
+          cy.get(falseSelector + ':checked').should('exist');
         } else {
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="ON"]').check();
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="ON"]:checked').should('exist');
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="OFF"]:checked').should('not.exist')
+          cy.get(trueSelector).check();
+          cy.get(trueSelector + ':checked').should('exist');
+          cy.get(falseSelector + ':checked').should('not.exist')
         }
-        cy.get('[data-flagpole="' + flagpoleNameText + '"]').find('.flagpole-value').contains(flagpoleValueText)
+        cy.get('[data-flagpole="' + flagpoleNameText + '"]').find('.flagpole-value')
+          .contains(flagpoleValueText)
       }
     })
 
     it("Use edit controls and update the value of each flagpole", function () {
       for (let i = 0; i < flagpoleKeys.length; i++) {
-        let flagpoleNameText = flagpoleKeys[i].toUpperCase(),
-          flagpoleValue = flagpoleData[flagpoleKeys[i]],
-          flagpoleValueReverseText = !flagpoleValue?'TRUE':'FALSE';
-        if (flagpoleValue) {
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="OFF"]').check();
-        } else {
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="ON"]').check();
-        }
-        cy.get('[data-flagpole="' + flagpoleNameText + '"]').find('.update-button').click().then(function() {
-          cy.get('[data-flagpole="' + flagpoleNameText + '"]').find('.flagpole-value').contains(flagpoleValueReverseText)
+        let flagpole = flagpoleData[flagpoleKeys[i]],
+          flagpoleNameText = flagpoleKeys[i].toUpperCase(),
+          flagpoleValue = flagpole.value,
+          flagpoleValueReverseText = !flagpole.value ? flagpole.trueName : flagpole.falseName,
+          flagpoleValueReverseTextDesc = !flagpole.value ? flagpole.trueDesc : flagpole.falseDesc,
+          selector = '[data-flagpole="' + flagpoleNameText + '"]';
+
+        cy.get(selector).find('.flagpole-mod-date').then(function(modDateEl) {
+          let origModDate=Date.parse(modDateEl.text()||'01 Jan 1970');
+          cy.log('Orig mod date :'+origModDate)
+
+          if (flagpoleValue) {
+            cy.get('[name="' + flagpoleNameText + '"][data-check-role="' + flagpole.falseName + '"]').check();
+          } else {
+            cy.get('[name="' + flagpoleNameText + '"][data-check-role="' + flagpole.trueName + '"]').check();
+          }
+
+          // Enforce a wait to make sure that the modification dates don't clash
+          cy.wait(1100)
+          cy.get(selector).find('.update-button').click().then(function () {
+            cy.get(selector).find('.flagpole-value').contains(flagpoleValueReverseText)
+            cy.get(selector).find('.flagpole-value-desc').contains(flagpoleValueReverseTextDesc)
+
+            cy.get(selector).find('.flagpole-mod-date').then(function(newModDateElem) {
+              let newModDate=Date.parse(newModDateElem.text());
+              cy.log('New mod date :'+newModDate)
+              expect(isNaN(newModDate)).to.be.false;
+              expect(newModDate).to.be.greaterThan(origModDate)
+            })
+
+          })
         })
       }
     })
 
+    it('Reread test data after update in last test ', function () {
+      cy.readFile('config.yaml').then((str) => {
+        const config = YAML.parse(str);
+        baseURL = config[env].domain;
+        cy.readFile(config[env]['source']).then(function (data) {
+          flagpoleData = data;
+          flagpoleKeys = Object.keys(flagpoleData);
+        })
+      })
+    })
+
     it("Use edit controls again to reverse the value of each flagpole", function () {
       for (let i = 0; i < flagpoleKeys.length; i++) {
-        let flagpoleNameText = flagpoleKeys[i].toUpperCase(),
-          flagpoleValue = flagpoleData[flagpoleKeys[i]],
-          flagpoleValueReverseText = !flagpoleValue?'TRUE':'FALSE';
-        if (flagpoleValue) {
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="OFF"]').check();
-        } else {
-          cy.get('[name="' + flagpoleNameText + '"][data-check-role="ON"]').check();
-        }
-        cy.get('[data-flagpole="' + flagpoleNameText + '"]').find('.update-button').click().then(function() {
-          cy.get('[data-flagpole="' + flagpoleNameText + '"]').find('.flagpole-value').contains(flagpoleValueReverseText)
+        let flagpole = flagpoleData[flagpoleKeys[i]],
+          flagpoleNameText = flagpoleKeys[i].toUpperCase(),
+          flagpoleValue = flagpole.value,
+          flagpoleValueReverseText = !flagpole.value ? flagpole.trueName : flagpole.falseName,
+          flagpoleValueReverseTextDesc = !flagpole.value ? flagpole.trueDesc : flagpole.falseDesc,
+          selector = '[data-flagpole="' + flagpoleNameText + '"]';
+
+        cy.get(selector).find('.flagpole-mod-date').then(function(modDateEl) {
+          let modificationDateString = modDateEl.text()
+          expect(modificationDateString).is.not("");
+          let origModDate=Date.parse(modificationDateString);
+
+          if (flagpoleValue) {
+            cy.get('[name="' + flagpoleNameText + '"][data-check-role="' + flagpole.falseName + '"]').check();
+          } else {
+            cy.get('[name="' + flagpoleNameText + '"][data-check-role="' + flagpole.trueName + '"]').check();
+          }
+
+          // Enforce a wait to make sure that the modification dates don't clash
+          cy.wait(1100);
+
+          cy.get(selector).find('.update-button').click().then(function () {
+            cy.get(selector).find('.flagpole-value').contains(flagpoleValueReverseText);
+            cy.get(selector).find('.flagpole-value-desc').contains(flagpoleValueReverseTextDesc)
+
+            cy.get(selector).find('.flagpole-mod-date').then(function (newModDateElem) {
+              let newModDate = Date.parse(newModDateElem.text());
+              expect(isNaN(newModDate)).to.be.false;
+              expect(newModDate).to.be.greaterThan(origModDate)
+            })
+          })
         })
       }
     })
